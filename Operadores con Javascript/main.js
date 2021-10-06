e67cbbaf4f9413556a7374e59e853327f91d0702
aiglesiasgesdek <aiglesias@gesdek.eus>
2021-10-06 16:55:13 +0200

    no message

--- Operadores con Javascript/main.js
+++ Operadores con Javascript/main.js
@@ -17,3 +17,43 @@ console.log(z);
 z = contador++;
 console.log(contador);
 console.log(z);
+
+//
+let a = 10;
+if(a % 2 == 0){
+    console.log("es par")
+}
+else{
+    console.log("es impar")
+}
+
+if(a % 3 == 0){
+    console.log("es par")
+}
+else{
+    console.log("es impar")
+}
+
+// And ---------------------------> &&
+
+// Or ----------------------------> ||
+
+//Operador ternario
+
+let resultado =  (3>2) ? "tres es mayor que dos" : "dos es mayor que tres";
+console.log(resultado)
+ 
+//Convertir string a integer
+let minumero = "10";
+console.log(typeof minumero);
+
+let edad = Number(minumero);
+console.log(typeof edad);
+
+//Isnan para saber si el tipo de la variable es numero IS NOT A NUMBER
+
+let casa = "casa";
+
+if( isNaN(casa) ){
+    console.log("no es numero");
+}
